f08ab8bcda3fd7ccce9c1befda6bbad33a328abf
Brady Kilpatrick <bkilpatrick3347@pointloma.edu>
2016-09-29 23:15:36 -0700

    added complete directory

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    private int score;
+
+    public GUIText gameOverText;
+    public GUIText restartText;
+    public GUIText scoreText;
+
+
+    private bool gameOver;
+    private bool restart;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        StartCoroutine(spawnWaves());
+    }
+    void updateScore()
+    {
+        scoreText.text = "Score " + score;
+    }
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+    IEnumerator spawnWaves()
+    {
+
+        yield return new WaitForSeconds(startWait);
+
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(-Random.Range(-spawnValues.x, spawnValues.x), 0.0f, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press R for restart";
+                restart = true;
+                break;
+            }
+        }
+
+
+    }
+    public void AddScore( int newscoreValue)
+    {
+        score += newscoreValue;
+        updateScore();
+    }
+    public void GameOver()
+    {
+        gameOverText.text = "GameOver";
+        gameOver = true;
+    }
+
+}
---
+++ README.txt
@@ -0,0 +1,4 @@
+For my unique feature I changed the fire key from the right click to a space bar
+ (I ran into an issue with WEBGL and the audio effects and didn't have time to set up the 
+complex hazard system I wanted to complete.... 
+probably still gonna do it just for fun. :)
